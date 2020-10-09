f11cab267a38ff44dcf61f4a98c14832d18563df
mako220 <kshsnh220@gmail.com>
2020-10-09 08:57:15 +0530

    lanes highlighted in still image

--- lanedetect.py
+++ lanedetect.py
@@ -1,6 +1,28 @@
 import cv2
+import numpy as np
 
 img = cv2.imread('lanedetect.png')
 
-cv2.imshow('result', img)
+height, width = img.shape[:2]
+
+img = cv2.resize(img,(width//2, height//2))
+
+grey = np.mean(img, axis=2).astype(np.uint8)
+
+blur = cv2.GaussianBlur(grey, (7, 7), 0)
+
+canny = cv2.Canny(blur, 50, 150)
+
+lines = cv2.HoughLinesP(canny, 2, np.pi/180, 50, np.array([]), minLineLength = 40, maxLineGap = 5)
+
+line_image = np.zeros_like(img)
+
+if lines is not None:
+    for line in lines:
+        x1, y1, x2, y2 = line.reshape(4)
+        cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+
+final_image = cv2.addWeighted(img, 0.7, line_image, 1, 1)
+#cv2.imshow('original', img)
+cv2.imshow('result', final_image)
 cv2.waitKey(0)
--- lanes.py
+++ lanes.py
@@ -3,18 +3,22 @@ import numpy as np
 
 
 cap = cv2.VideoCapture('lanedetect.mp4')
+width = int(cap.get(3))
+height = int(cap.get(4))
+fps = cap.get(5)
+print(fps)
 
 cv2.namedWindow("Frame")
 
 while(cap.isOpened()):
     _, frame = cap.read()
 
-    img = cv2.resize(frame , (1920//2,1080//2))
+    img = cv2.resize(frame , (width//2,height//2))
 
-    gray = np.mean(img, axis=2).astype(np.uint8)
+    grey = np.mean(img, axis=2).astype(np.uint8)
     #print(img.shape)
     #print(img)
-    cv2.imshow("Frame", img)
+    cv2.imshow("Frame", grey)
     key = cv2.waitKey(1)
     if key == 27:
          break
