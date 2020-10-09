1642c5e9f40c995aa66b00ad046d06342bc47008
mako220 <kshsnh220@gmail.com>
2020-10-09 21:07:51 +0530

    added region of interest

--- lanedetect.py
+++ lanedetect.py
@@ -1,5 +1,6 @@
 import cv2
 import numpy as np
+#import matplotlib.pyplot as plt
 
 img = cv2.imread('lanedetect.png')
 
@@ -9,11 +10,37 @@ img = cv2.resize(img,(width//2, height//2))
 
 grey = np.mean(img, axis=2).astype(np.uint8)
 
+height1, width1 = grey.shape[:2]
+
+print("height : ", height1, " width : ", width1)
+
 blur = cv2.GaussianBlur(grey, (7, 7), 0)
 
 canny = cv2.Canny(blur, 50, 150)
 
-lines = cv2.HoughLinesP(canny, 2, np.pi/180, 50, np.array([]), minLineLength = 40, maxLineGap = 5)
+roi_poly = np.array([
+[(349, 131), (638, 131), (728, 261), (292, 261)]
+], 'int32')
+
+#mask2 = np.zeros_like(grey)
+
+#print(mask2.dtype)
+
+mask = np.zeros((height1, width1), dtype = np.uint8)
+
+#print(mask.dtype)
+
+cv2.fillPoly(mask, roi_poly, (255, 255, 255))
+
+#cv2.imshow('mask', mask)
+#cv2.waitKey(0)
+
+roi = cv2.bitwise_and(canny, mask)
+
+#cv2.imshow('masked_image', roi)
+#cv2.waitKey(0)
+
+lines = cv2.HoughLinesP(roi, 2, np.pi/180, 50, np.array([]), minLineLength = 40, maxLineGap = 5)
 
 line_image = np.zeros_like(img)
 
@@ -26,3 +53,5 @@ final_image = cv2.addWeighted(img, 0.7, line_image, 1, 1)
 #cv2.imshow('original', img)
 cv2.imshow('result', final_image)
 cv2.waitKey(0)
+#plt.imshow(final_image)
+#plt.show( )
--- lanes.py
+++ lanes.py
@@ -8,6 +8,22 @@ height = int(cap.get(4))
 fps = cap.get(5)
 print(fps)
 
+
+roi_poly = np.array([
+[(349, 131), (638, 131), (728, 261), (292, 261)]
+], 'int32')
+
+#mask2 = np.zeros_like(grey)
+
+#print(mask2.dtype)
+
+mask = np.zeros((540, 960), dtype = np.uint8)
+
+#print(mask.dtype)
+
+cv2.fillPoly(mask, roi_poly, (255, 255, 255))
+
+
 cv2.namedWindow("Frame")
 
 while(cap.isOpened()):
@@ -16,9 +32,29 @@ while(cap.isOpened()):
     img = cv2.resize(frame , (width//2,height//2))
 
     grey = np.mean(img, axis=2).astype(np.uint8)
-    #print(img.shape)
-    #print(img)
-    cv2.imshow("Frame", grey)
+
+    print(grey.shape)
+
+    #grey = np.mean(img, axis=2).astype(np.uint8)
+
+    blur = cv2.GaussianBlur(grey, (7, 7), 0)
+
+    canny = cv2.Canny(blur, 50, 150)
+
+    roi = cv2.bitwise_and(canny, mask)
+
+    lines = cv2.HoughLinesP(roi, 2, np.pi/180, 50, np.array([]), minLineLength = 40, maxLineGap = 5)
+
+    line_image = np.zeros_like(img)
+
+    if lines is not None:
+        for line in lines:
+            x1, y1, x2, y2 = line.reshape(4)
+            cv2.line(line_image, (x1, y1), (x2, y2), (255, 0, 0), 10)
+
+    final_image = cv2.addWeighted(img, 0.7, line_image, 1, 1)
+
+    cv2.imshow("Frame", final_image)
     key = cv2.waitKey(1)
     if key == 27:
          break
